9d51e75e6ce1d8fdfa5bfc47a86b50cbc3b90a77
sr-guerra <pdsguerra@gmail.com>
2023-03-31 19:10:34 -0300

    inicio de novos exercícios

---
+++ estrutura-de-decisao/ex2.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+num = int(input('Digite um número inteiro:'))
+
+if num >= 0:
+    print(f'Este número {num} é positivo.')
+else:
+    print(f'Este número {num} é negativo.')
---
+++ estrutura-de-decisao/ex3.py
@@ -0,0 +1,6 @@
+letra = input('Digite "M" ou "F".')
+
+if letra == 'M':
+    print('Masculino')
+if letra == 'F':
+    print('Feminino')
